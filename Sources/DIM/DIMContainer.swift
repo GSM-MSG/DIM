c214b9fadb1a2f9ad3551628425f8e6a2cf165ed
baegteun <baegteun@gmail.com>
2023-01-10 16:25:24 +0900

    :memo: :: DIM Docs

--- Sources/DIM/DIMContainer.swift
+++ Sources/DIM/DIMContainer.swift
@@ -1,47 +1,47 @@
 import Foundation
 
+/// DIMContainer is a class that stores dependencies and allows you to resolve them by name
 public final class DIMContainer {
+    /// The root container is a singleton that is shared across the entire app
     private static let root = DIMContainer()
-    private var injected: [String: Injection] = [:]
+    /// A dictionary that maps injection names to Injection objects
+    internal private(set) var injected: [String: Injection] = [:]
 
+    /// Initializes an empty container
     public init() {}
 
+    /// Initializes a container with an array of Injections
     public init(@InjectionBuilder _ injections: () -> [Injection]) {
         injections().forEach { register(injection: $0) }
     }
 
-    public init(@InjectionBuilder _ injection: () -> Injection) {
-        register(injection: injection())
-    }
-
-    public init(@DIMContainerBuilder _ container: () -> DIMContainer) {
-        container().injected.forEach {
-            register(injection: $0.value)
-        }
-    }
-
-    public init (@DIMContainerBuilder _ containers: () -> [DIMContainer]) {
-        containers().forEach {
-            $0.injected.forEach {
-                register(injection: $0.value)
-            }
-        }
-    }
-
+    /// Clears the dependencies from the container when it is deinitialized
     deinit { injected.removeAll() }
 
-    static func resolve<T>(for name: String? = nil) -> T {
+    /// Returns an instance of type T from the root container, based on the optional name parameter
+    public static func resolve<T>(for name: String? = nil) -> T {
         let name = name ?? String(describing: T.self)
-        guard let resolved = DIMContainer.root.injected[name] as? T else {
+        guard let resolved = DIMContainer.root.injected[name]?.resolve() as? T else {
             fatalError("\(name) not registered")
         }
         return resolved
     }
 
+    /// Returns an instance of type T from the root container, based on the optional Type parameter
+    public static func resolve<T>(for type: T.Type) -> T {
+        let name = String(describing: T.self)
+        guard let resolved = DIMContainer.root.injected[name]?.resolve() as? T else {
+            fatalError("\(name) not registered")
+        }
+        return resolved
+    }
+
+    /// Registers an Injection object in the container
     public func register(injection: Injection) {
         injected[injection.name] = injection
     }
 
+    /// Sets the injected dictionary of the root container to the current container's injected dictionary
     public func build() {
         DIMContainer.root.injected = self.injected
     }
--- Sources/DIM/InjectionBuilder.swift
+++ Sources/DIM/InjectionBuilder.swift
@@ -1,16 +1,22 @@
 import Foundation
 
+/// InjectionBuilder is a result builder that allows you to build an array of Injections
+/// using the @InjectionBuilder property wrapper
 @resultBuilder
-struct InjectionBuilder {
-    static func buildBlock(_ components: Injection...) -> [Injection] { components }
+public struct InjectionBuilder {
+    public static func buildBlock(_ components: Injection...) -> [Injection] { components }
 
-    static func buildBlock(_ components: [Injection]) -> [Injection] { components }
+    public static func buildBlock(_ components: Injection) -> [Injection] { [components] }
+    
+    public static func buildBlock(_ components: DIMContainer...) -> [Injection] {
+        components.map(\.injected).reduce(into: [Injection]()) { partialResult, injected in
+            partialResult += injected.values
+        }
+    }
 
-    static func buildBlock(_ components: Injection) -> Injection { components }
+    public static func buildEither(first component: Injection) -> Injection { component }
 
-    static func buildEither(first component: Injection) -> Injection { component }
+    public static func buildEither(second component: Injection) -> Injection { component }
 
-    static func buildEither(second component: Injection) -> Injection { component }
-
-    static func buildLimitedAvailability(_ component: Injection) -> Injection { component }
+    public static func buildLimitedAvailability(_ component: Injection) -> Injection { component }
 }
